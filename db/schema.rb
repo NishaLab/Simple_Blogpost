24e241908d723ba0756e7ce4a405e765fcc19dd0
hungnd <hungnguyenw420@gmail.com>
2020-09-28 14:22:23 +0700

    update seeds

--- db/schema.rb
+++ db/schema.rb
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20_200_928_030_706) do
     t.integer 'user_id'
     t.datetime 'created_at', precision: 6, null: false
     t.datetime 'updated_at', precision: 6, null: false
+    t.index %w[user_id created_at], name: 'index_microposts_on_user_id_and_created_at'
   end
 
   create_table 'users', force: :cascade do |t|
