2ceac3d13aee12e05251ea2ceeedbcb8d5ef0744
hungnd <hungnguyenw420@gmail.com>
2020-09-29 09:46:40 +0700

    add follow unfollow button

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,7 @@ Rails.application.routes.draw do
       get :following, :followers
     end
   end
+  resources :relationships, only: %i[create destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
 end
