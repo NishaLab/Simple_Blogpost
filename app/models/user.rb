f16029c1f4a1f47ab0c07bd8f62d04862e378e61
hungnd <hungnguyenw420@gmail.com>
2020-09-29 08:50:59 +0700

    add relationship model

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,10 +8,13 @@ class User < ApplicationRecord
   before_create :create_activation_digest
 
   has_many :microposts, dependent: :destroy
+  has_many :active_relationships, class_name: 'Relationship',
+                                  foreign_key: 'follower_id', dependent: :destroy
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i.freeze
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: true
-  validates :password, presence: true, format: { with: /\A[A-Z]+[a-z]+[0-9]+/ }, length: { maximum: 58, minimum: 8 }
+  validates :password, presence: true, format: { with: /\A[A-Z]+[a-z]+[0-9]+/ },
+                       length: { maximum: 58, minimum: 8 }
   has_secure_password
 
   def self.digest(string)
