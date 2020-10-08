562abf03d25d9592a3efa77fd36154f162ae0514
hungnd <hungnguyenw420@gmail.com>
2020-10-08 10:03:50 +0700

    spacing

--- spec/factories/factories.rb
+++ spec/factories/factories.rb
@@ -3,22 +3,23 @@ FactoryBot.define do
     sequence(:email) { |n| "emailtest-#{n}@gmail.com" }
     password { "12345678" }
     password_confirmation { "12345678" }
-    name {"hung1234"}
-    activated {true}
-    activated_at {Time.zone.now}
+    name { "hung1234" }
+    activated { true }
+    activated_at { Time.zone.now }
   end
 
   factory :micropost do
     user
-    content {"asfasd"}
-    user_id {user.id}
+    content { "asfasd" }
+    user_id { user.id }
+    parent_id { nil }
   end
 
   factory :reaction do
     user
     micropost
-    image_id {"1"}
-    user_id {user.id}
-    micropost_id {micropost.id}
+    image_id { "1" }
+    user_id { user.id }
+    micropost_id { micropost.id }
   end
 end
