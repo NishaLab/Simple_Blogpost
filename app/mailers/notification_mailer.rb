c3c4cb9ed2f839d64e209a5e1ad17f38404b2a5c
hungnd <hungnguyenw420@gmail.com>
2020-10-19 16:41:18 +0700

    update

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,7 +1,6 @@
 class NotificationMailer < ApplicationMailer
   def new_notification reaction
     @reaction = reaction
-    binding.pry
     mail(to: reaction.micropost.user.email, subject: "You got a new notification!")
   end
 end
