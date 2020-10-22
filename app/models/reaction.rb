db21fd923c65bf6b7cdc7d6a3fd83fe93f0d7c76
hungnd <hungnguyenw420@gmail.com>
2020-10-22 14:25:49 +0700

    rubocop

--- app/models/reaction.rb
+++ app/models/reaction.rb
@@ -5,11 +5,11 @@ class Reaction < ApplicationRecord
   scope :all_notification, ->(user_id) {
     post_ids = Micropost.where(user_id: user_id).pluck(:id)
     self.where(micropost_id: post_ids)
-   }
-   scope :all_unread_notification, ->(user_id) {
+  }
+  scope :all_unread_notification, ->(user_id) {
     post_ids = Micropost.where(user_id: user_id).pluck(:id)
     self.where(micropost_id: post_ids, is_read: false)
-   }
+  }
   belongs_to :user
   belongs_to :micropost
   validates :user_id, presence: true
