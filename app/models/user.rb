16ec1f29702099eb3f5c82f17b580a90b6791ee4
hungnd <hungnguyenw420@gmail.com>
2020-10-26 08:47:57 +0700

    init devise + change reset password to devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,10 @@
 
 # dasdasda
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   attr_accessor :remember_token, :activation_token, :reset_token
 
   before_save { self.email = email.downcase }
