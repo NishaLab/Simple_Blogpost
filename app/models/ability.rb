96462096f9393aa77be4e32b0c1f7a1c5a2c662e
hungnd <hungnguyenw420@gmail.com>
2020-10-26 09:08:04 +0700

    cancancan guest can sign up

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -6,24 +6,28 @@ class Ability
   def initialize(user)
     alias_action :create, :read, :update, :destroy, to: :crud
 
-    # User privilege for Micropost
-    can :read, Micropost
-    can :manage, Micropost, user_id: user.id
-    if user.admin?
-      can :manage, Micropost
-    end
-    #User privilege for User model
-    can :show, User
-    can [:update, :destroy, :export], User, id: user.id
-    if user.admin?
-      can :manage, User
-    end
+    unless user.nil?
+      # User privilege for Micropost
+      can :read, Micropost
+      can :manage, Micropost, user_id: user.id
+      if user.admin?
+        can :manage, Micropost
+      end
+      #User privilege for User model
+      can :show, User
+      can [:update, :destroy, :export], User, id: user.id
+      if user.admin?
+        can :manage, User
+      end
 
-    #User privilage for Reaction Model
-    can :crud, Reaction
-    
-    #User privilage to Relationship Model
-    can [:read, :create], Relationship
-    can [:update, :destroy], Relationship, follower_id: user.id
+      #User privilage for Reaction Model
+      can :crud, Reaction
+
+      #User privilage to Relationship Model
+      can [:read, :create], Relationship
+      can [:update, :destroy], Relationship, follower_id: user.id
+    else
+      can :new , User
+    end
   end
 end
