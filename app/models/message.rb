179f2da13e2706d297cea3377d5ccb76fac9bd6b
hungnd <hungnguyenw420@gmail.com>
2020-11-02 16:13:43 +0700

    chat window view

--- app/models/message.rb
+++ app/models/message.rb
@@ -4,5 +4,8 @@ class Message < ApplicationRecord
   belongs_to :receiver, class_name: "User"
   validates :sender_id, presence: true
   validates :receiver_id, presence: true
+
+  scope :current_user_messages, ->(user_id) { where(sender_id: user_id).or(self.where(receiver_id: user_id))}
+
 end
 
