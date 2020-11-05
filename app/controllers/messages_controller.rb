ab7bb0a090cf709daf91886af21f0505d463a974
hungnd <hungnguyenw420@gmail.com>
2020-11-05 11:06:47 +0700

    fix comment

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,8 +1,4 @@
 class MessagesController < ApplicationController
-  def index
-    @messages = Message.all
-  end
-
   def create
     @message = Message.new(sender_id: params[:sender_id], receiver_id: params[:receiver_id],
                            content: params[:message][:content])
