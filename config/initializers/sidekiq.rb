a94994f62f5465c8ea77790ecfe6d26a741b1d5c
hungnd <hungnguyenw420@gmail.com>
2020-11-12 14:13:37 +0700

    set up for heroku

---
+++ config/initializers/sidekiq.rb
@@ -0,0 +1,9 @@
+Sidekiq.configure_client do |config|
+  config.redis = { url: ENV.fetch("REDIS"), size: 1, network_timeout: 5 }
+end
+
+Sidekiq.configure_server do |config|
+  config.redis = { url: ENV.fetch("REDIS"), size: 12, network_timeout: 5 }
+end
+
+Sidekiq::Extensions.enable_delay!
