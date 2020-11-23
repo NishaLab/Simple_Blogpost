dfaa8bb8f3fecda49bd93becd737a3a65bf16f00
hungnd <hungnguyenw420@gmail.com>
2020-11-23 15:55:13 +0700

    update

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
 set :rails_env, :production
 set :deploy_to, "/var/www/blog_ruby_server"
 set :branch, :redeploy
-server "3.0.100.238", user: "deploy", roles: %w(web app db)
+server "3.1.218.64", user: "deploy", roles: %w(web app db)
