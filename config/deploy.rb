46dafde69656598515b7b4fc755d7befc3b5552c
hungnd <hungnguyenw420@gmail.com>
2020-11-23 15:25:52 +0700

    update

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,59 +9,17 @@ set :linked_dirs, %w(log tmp/pids tmp/cache tmp/sockets vendor/bundle public/sys
 set :keep_releases, 5
 set :rvm_type, :user
 
-set :pty,             true
-set :use_sudo,        false
-set :stage,           :production
-set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
-set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
-set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
-set :puma_access_log, "#{release_path}/log/puma.error.log"
-set :puma_error_log,  "#{release_path}/log/puma.access.log"
-set :puma_preload_app, true
+set :puma_rackup, -> {File.join(current_path, "config.ru")}
+set :puma_state, -> {"#{shared_path}/tmp/pids/puma.state"}
+set :puma_pid, -> {"#{shared_path}/tmp/pids/puma.pid"}
+set :puma_bind, "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
+set :puma_conf, -> {"#{shared_path}/puma.rb"}
+set :puma_access_log, -> {"#{shared_path}/log/puma_access.log"}
+set :puma_error_log, -> {"#{shared_path}/log/puma_error.log"}
+set :puma_role, :app
+set :puma_env, fetch(:rack_env, fetch(:rails_env, "production"))
+set :puma_threads, [0, 8]
+set :puma_workers, 1
 set :puma_worker_timeout, nil
-set :puma_init_active_record, true  # Change to true if using ActiveRecord
-
-namespace :puma do
-  desc 'Create Directories for Puma Pids and Socket'
-  task :make_dirs do
-    on roles(:app) do
-      execute "mkdir #{shared_path}/tmp/sockets -p"
-      execute "mkdir #{shared_path}/tmp/pids -p"
-    end
-  end
-
-  before :start, :make_dirs
-end
-
-namespace :deploy do
-  desc "Make sure local git is in sync with remote."
-  task :check_revision do
-    on roles(:app) do
-      unless `git rev-parse HEAD` == `git rev-parse origin/master`
-        puts "WARNING: HEAD is not the same as origin/master"
-        puts "Run `git push` to sync changes."
-        exit
-      end
-    end
-  end
-
-  desc 'Initial Deploy'
-  task :initial do
-    on roles(:app) do
-      before 'deploy:restart', 'puma:start'
-      invoke 'deploy'
-    end
-  end
-
-  desc 'Restart application'
-  task :restart do
-    on roles(:app), in: :sequence, wait: 5 do
-      invoke 'puma:restart'
-    end
-  end
-
-  before :starting,     :check_revision
-  after  :finishing,    :compile_assets
-  after  :finishing,    :cleanup
-  after  :finishing,    :restart
-end
+set :puma_init_active_record, true
+set :puma_preload_app, false
